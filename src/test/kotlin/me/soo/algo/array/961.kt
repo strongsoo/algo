e3d4018c3c5b7a8daff89719abc8a45a4c1f8ea0
김환수 <hwansoo.kim@navercorp.com>
2021-01-10 10:10:58 +0900

    Add leetcode 961

---
+++ src/test/kotlin/me/soo/algo/array/961.kt
@@ -0,0 +1,26 @@
+package me.soo.algo.array
+
+import org.amshove.kluent.shouldBeEqualTo
+import org.junit.jupiter.api.Test
+
+internal class `961` {
+
+    @Test
+    fun test() {
+        repeatedNTimes(intArrayOf(1, 2, 3, 3)) shouldBeEqualTo 3
+        repeatedNTimes(intArrayOf(2, 1, 2, 5, 3, 2)) shouldBeEqualTo 2
+        repeatedNTimes(intArrayOf(5, 1, 5, 2, 5, 3, 5, 4)) shouldBeEqualTo 5
+    }
+
+    fun repeatedNTimes(A: IntArray): Int {
+        val counterArray = IntArray((A.size / 2) + 1) { 0 }
+
+        A.forEachIndexed { _, v ->
+            if (++counterArray[v - 1] >= 2) {
+                return v
+            }
+        }
+
+        return 0
+    }
+}
